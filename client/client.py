724e0c0ddfa9e6718ed075182b38166832edbed1
magus96 <anirudh5462@gmail.com>
2024-03-07 16:00:42 +0530

    Added LRU Cache functionality

--- client/client.py
+++ client/client.py
@@ -1,15 +1,27 @@
 import Pyro4
 
 def main():
-    cache_uri = "PYRO:obj_76de72a42d15448281934daf92959312@localhost:38403"
+    cache_uri = "PYRO:obj_80237c128c7d43599e1ac9c4840b95a1@localhost:46801"
 
     cache_server = Pyro4.Proxy(cache_uri)
 
-    cache_server.set(1, "Anirudh")
-    value = cache_server.get(1)
-    print(value)
-    value = cache_server.get(2)
-    print(value)
+    for i in range(29):
+        print(i)
+        cache_server.set(i, "Anirudh")
+        if i == 26:
+            val = cache_server.get(1)
+            print(val)
+            if val is None:
+                print("Cache Miss")
+        value = cache_server.get(i)
+
+        if value is None:
+            print('Cache miss')
+        else:
+            print('Cache hit')
 
 if __name__== "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        print("".join(Pyro4.util.getPyroTraceback()))
--- server/cacher.py
+++ server/cacher.py
@@ -1,12 +1,19 @@
 import Pyro4
+CACHE_LEN = 16
 
 @Pyro4.expose
 class Cacher:
     def __init__(self):
         self.cache = {}
+        self.eles = []
     
     def set(self, key, value):
         self.cache[key] = value
+        if len(self.eles) > 16:
+            del self.cache[self.eles[0]]
+            self.eles.pop(0)
+            self.eles.append(key)
+        self.eles.append(key)
 
     def get(self, key):
         value = self.cache.get(key)
